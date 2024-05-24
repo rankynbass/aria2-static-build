4d6bcadaa141493bf7fb0dbb5840bbc2b936b461
Rankyn Bass <rankyn@proton.me>
2024-05-23 18:51:05 -0700

    First edit to build script

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash -e
 
-# This scrip is for static cross compiling
-# Please run this scrip in docker image: abcfy2/muslcc-toolchain-ubuntu:${CROSS_HOST}
-# E.g: docker run --rm -v `git rev-parse --show-toplevel`:/build abcfy2/muslcc-toolchain-ubuntu:arm-linux-musleabi /build/build.sh
-# Artifacts will copy to the same directory.
+# For peace of mind, we'll use a default ubuntu:24.04 container and add the musl toolchain here instead
+export ARCH=x86_64-linux-musl
+export CROSS_HOST="${ARCH}"
+export CROSS_ROOT="/cross_root"
+export PATH="${CROSS_ROOT}/bin:${PATH}"
+export CROSS_PREFIX="${CROSS_ROOT}/${CROSS_HOST}"
+export DEBIAN_FRONTEND=noninteractive
+
+mkdir -p "${CROSS_ROOT}"
+apt update
+apt install -y curl
+curl -SLfA "MacroMu" -o "/tmp/${ARCH}-cross.tgz" "https://musl.cc/${ARCH}-cross.tgz"
+SHA512SUMS="$(curl -sSLfA "MacroMu" --compressed https://musl.cc/SHA512SUMS)"
+SHA512SUM="$(echo "${SHA512SUMS}" | grep "${CROSS_HOST}-cross.tgz" | head -1)"
+cd /tmp
+echo "${SHA512SUM}" | sha512sum -c
+tar -zxf "/tmp/${ARCH}-cross.tgz" --transform='s|^\./||S' --strip-components=1 -C "${CROSS_ROOT}"
+rm -f "/tmp/${ARCH}-cross.tgz"
+echo "${SHA512SUM}" > /SHA512SUM.txt
 
 set -o pipefail
 
